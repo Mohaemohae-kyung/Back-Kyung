2e057a64496c753bc89cf89df7e6f2a21a1a09ae
MinJunKKang <kmg4613@naver.com>
2026-05-17 17:41:40 +0900

    초기 세팅 및 의존성 설정

---
+++ src/main/java/kyung/kung_backend/KungBackendApplication.java
@@ -0,0 +1,13 @@
+package kyung.kung_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KungBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KungBackendApplication.class, args);
+	}
+
+}
