eab71a7b4fdd1278af9ae577483be0a5f05ceb5f
MinJunKKang <kmg4613@naver.com>
2026-05-18 23:04:29 +0900

    chore: 지역 초기 데이터 설정 추가

--- src/main/java/kyung/kung_backend/domain/location/repository/LocationRepository.java
+++ src/main/java/kyung/kung_backend/domain/location/repository/LocationRepository.java
@@ -3,5 +3,18 @@ package kyung.kung_backend.domain.location.repository;
 import kyung.kung_backend.domain.location.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LocationRepository extends JpaRepository<Location, Long> {
+
+    Optional<Location> findByNameAndDepthAndParentIsNull(
+            String name,
+            Long depth
+    );
+
+    Optional<Location> findByNameAndParentAndDepth(
+            String name,
+            Location parent,
+            Long depth
+    );
 }
