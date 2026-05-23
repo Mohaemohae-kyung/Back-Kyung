9ae9e9eb71a2cbbc0deb44efdef160798d5ecca7
Ragdo11s <wsshin0517@gmail.com>
2026-05-23 17:37:13 +0900

    백엔드 컨트롤러 작성

---
+++ src/main/java/kyung/kung_backend/domain/integrity/controller/AppIntegrityController.java
@@ -0,0 +1,26 @@
+package your.package.integrity.controller;
+
+import your.package.integrity.dto.AppIntegrityReportRequest;
+import your.package.integrity.dto.AppIntegrityReportResponse;
+import your.package.integrity.service.AppIntegrityService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/app-integrity")
+public class AppIntegrityController {
+
+    private final AppIntegrityService appIntegrityService;
+
+    public AppIntegrityController(AppIntegrityService appIntegrityService) {
+        this.appIntegrityService = appIntegrityService;
+    }
+
+    @PostMapping("/report")
+    public ResponseEntity<AppIntegrityReportResponse> report(
+            @RequestBody AppIntegrityReportRequest request
+    ) {
+        AppIntegrityReportResponse response = appIntegrityService.evaluate(request);
+        return ResponseEntity.ok(response);
+    }
+}
