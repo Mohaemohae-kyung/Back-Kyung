1568fd1a6a44c2c4b614fbde0eaae0e506cf2a50
MinJunKKang <kmg4613@naver.com>
2026-05-28 01:02:31 +0900

    fix: 충돌 문제로 인한 main 코드 재삽입 (Booking.java, BookingController.java)

--- src/main/java/kyung/kung_backend/domain/booking/entity/Booking.java
+++ src/main/java/kyung/kung_backend/domain/booking/entity/Booking.java
@@ -1,6 +1,15 @@
 package kyung.kung_backend.domain.booking.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import kyung.kung_backend.domain.location.entity.Location;
 import kyung.kung_backend.domain.store.entity.StoreProduct;
 import kyung.kung_backend.domain.user.entity.User;
